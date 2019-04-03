2143984dc06bd4acdb4a116355d48b2b5b4da6d3
b <bslucena@gmail.com>
2019-04-03 12:02:58 +0200

    first commit

---
+++ iA - Bullseye/AboutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutViewController.swift
+//  iA - Bullseye
+//
+//  Created by Bernardo de Lucena on 29/11/2018.
+//  Copyright © 2018 Bernardo de Lucena. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
